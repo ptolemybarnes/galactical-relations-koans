b69aab7b547a7dc330ce2c070f45f1bdd9f5260d
Ptolemy <ptolemybarnes@gmail.com>
2015-08-03 13:41:04 +0100

    Makes syntax more consistent in sun_spec

--- lib/tasks/galactical_koans.rake
+++ lib/tasks/galactical_koans.rake
@@ -34,5 +34,3 @@ namespace :galaxy do
   
 end
 
-
-
--- spec/models/sun_spec.rb
+++ spec/models/sun_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Sun, stage: :alpha do
 
   it 'is circled by many planets', stage: :alpha do
-    expect(subject).to have_many(:planets)
+    expect(subject).to have_many :planets
   end
 
   it 'is circled by many moons, via planets', stage: :delta do
