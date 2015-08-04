27158a3ddf823b4061480a24f938179f2b8567ad
Ptolemy <ptolemybarnes@gmail.com>
2015-08-04 10:28:01 +0100

    Supplements rake helpers with methods for formatting strings

--- lib/tasks/rake_helpers.rb
+++ lib/tasks/rake_helpers.rb
@@ -4,4 +4,16 @@ module RakeHelpers
   end
 end
 
+class String
+
+  def colourize
+    "\033[1;31m#{self}\033[0m...\n"
+  end
+
+  def space(value)
+    spaces = "\n" * value
+    spaces + self + spaces
+  end
+
+end
 
