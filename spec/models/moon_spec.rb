88d78b60810c703b475aff456e2c9045c3accb1e
Natalie Akam <natalie.akam@gmail.com>
2015-08-07 16:05:24 +0100

    Update moon_spec.rb
    Correct wording of test

--- spec/models/moon_spec.rb
+++ spec/models/moon_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Moon do
     expect(subject).to belong_to :planet
   end
 
-  it 'has a single moon', stage: :delta do
+  it 'has a single sun', stage: :delta do
     expect(subject).to have_one :sun 
   end
 
