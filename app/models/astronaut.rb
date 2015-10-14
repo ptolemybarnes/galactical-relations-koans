643cd35d668d1417f842211964cb9e05bcb39472
Ptolemy <ptolemybarnes@gmail.com>
2015-10-14 10:53:44 +0100

    Removes solution from astronaut and spaceship classes

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -1,8 +1,2 @@
 class Astronaut < ActiveRecord::Base
-  has_many :crewings
-  has_many :spaceships, through: :crewings
-
-  def spaceship
-    crewings.where(captain: true).first.spaceship
-  end
 end
--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,8 +1,2 @@
 class Spaceship < ActiveRecord::Base
-  has_many :crewings
-  has_many :astronauts, through: :crewings
-
-  def captain
-    crewings.where(captain: true).first.astronaut
-  end
 end
